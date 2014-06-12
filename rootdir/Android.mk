77ebd6a3a9079bd0a63a20e6efcfe69f773d3dd8
Jake Whatley <jacob.whatley91@gmail.com>
2014-06-11 20:57:07 -0400

    jflte: bring back optimizations from before unification
    - compcache
    - remove mpdecision
      - set BOOT_SOCKET path
    - remove OTG Test from init.target.rc
    
    Change-Id: I05f173a0e4f86598c1b37ce7c56d88620a08805b

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -56,3 +56,11 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.crda.sh
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := init.local.rc
+LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := etc/init.local.rc
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
