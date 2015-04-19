c4cab74b76513fdea5bed510440de33e05a5e1ee
Jake Whatley <jacob.whatley91@gmail.com>
2015-04-19 17:11:35 -0400

    jflte: move ril class to device
    Change-Id: I5c34e90e901a5a5996b6174dcbe5ab8a1b393336

--- device.mk
+++ device.mk
@@ -185,6 +185,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.telephony.ril.v3=newDriverCall \
     dalvik.vm.dexopt-data-only=0
 
+# RIL
+    ro.telephony.ril_class=SamsungQualcommRIL
+
 # Smart Cover
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.switch_code.sw_lid=0x15 \
